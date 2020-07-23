aea252a4fb9effd45011d24522ae7063f857422d
Ico <tagot.nicolas@gmail.com>
2020-07-23 16:13:41 +0200

    petite modif

--- Routes/post.js
+++ Routes/post.js
@@ -1,13 +1,9 @@
-var cors = require('cors')
+var cors = require('cors');
 require('dotenv').config();
 const postRouter = require('express').Router();
 const connection = require('../conf.js');
-const bcrypt = require('bcrypt');
-const { send } = require('process');
-const saltRounds = 3;
 
 postRouter.get('/all', (req, res) => {
-      
     connection.query("SELECT post.id, post.title, post.intro, post.burn, post.cold FROM post JOIN depends ON post.depend = depends.id ", (err, results) => {
       if(err){
         res.status(404).send("Not found");
@@ -21,43 +17,40 @@ postRouter.get('/all', (req, res) => {
 
   postRouter.get('/who/:who', (req, res) => {
     const { who } = req.params;
-    console.log(who)
     connection.query(`SELECT post.id, post.title, post.intro, burn, cold FROM post JOIN depends ON post.depend = depends.id ORDER BY ${who} DESC`, (err, results) => {
       if(err){
         res.status(404).send("Not found");
         console.log(err)
       } else {
-        console.log(who)
-        res.status(200).json(results)
+        res.status(200).json(results);
           }
     })
   })
 
   postRouter.get('/by/:who', (req, res) => {
     const { who } = req.params;
-    console.log(who)
+    console.log(who);
     connection.query("SELECT post.id, post.title, post.intro, post.burn, post.cold FROM post JOIN depends ON post.depend = depends.id WHERE depends.id = ?", who, (err, results) => {
       if(err){
         res.status(404).send("Not found");
-        console.log(err)
+        console.log(err);
       } else {
-        console.log(results)
-        res.status(200).json(results)
+        res.status(200).json(results);
           }
     })
   })
 
   postRouter.get('/article/:id', (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
     connection.query('SELECT title, intro, content, cold, burn FROM post WHERE id = ?',id, (err, results) => {
       if(err){
         res.status(404).send("Not found...");
-        console.log(err)
+        console.log(err);
       } else {
         res.status(200).json(results[0]);
       }
-    })
-  })
+    });
+  });
 
   postRouter.put('/modify/:id',  cors(), (req, res, next) => {
     const { id } = req.params;
@@ -65,37 +58,28 @@ postRouter.get('/all', (req, res) => {
     connection.query("UPDATE post SET ? WHERE id = ?", [body, id], (err, results)=>{
       if(err){
         console.log(err)
-        console.log(body)
         res.status(500).send('Server error...');
     } else {
-      console.log(id)
-      console.log(body)
          res.status(201).send("it's done!");
     }
-    })
-  })
+    });
+  });
 
   postRouter.post('/delete/:id',  cors(), (req, res, next) => {
     const { id } = req.params;
     const { name, key } = req.body;
-    console.log(id)
-    console.log(req.body)
     connection.query("DELETE from post where user = ? AND id = ? AND key_post = ?",[name, id, key], (err,results) => {
      if(err){
        console.log(err)
-       res.status(500).send('Erreur de champ...')
+       res.status(500).send('Erreur de champ...');
      } else {
-        console.log(name, key)
-       res.status(200).send('Deleted !')
+       res.status(200).send('Deleted !');
      }
-      })
+      });
 
-    })
-
+    });
 
   postRouter.post('/add', (req, res) => {
-  
-    console.log(req.body);
     const toAdd  = req.body
     connection.query("INSERT INTO post SET ?", toAdd, (err, results) => {
     if(err){
@@ -106,8 +90,5 @@ postRouter.get('/all', (req, res) => {
     }
   });
 });
-
-// DELETE requete.
-
 
 module.exports = postRouter;
--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const port = 8000;
 const router = express.Router();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-
     app.options('*', (req, res) => {
         // allowed XHR methods
         res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
@@ -22,7 +18,7 @@ const aboutRoute = require('./Routes/about');
 const dependRoute = require('./Routes/depend');
 const postRoute = require('./Routes/post');
 const adminRoute = require('./Routes/admin');
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 
@@ -32,5 +28,5 @@ app.use('/post', postRoute);
 app.use('/admin', adminRoute);
 
 app.listen(port, (res, req) => {
-  console.log("We are here => " + port);
+  console.log("Politiq burn on: " + port);
 });
